4f60dfcd7ef6ddd8cbfdcb98f96984ffd81723e2
Mark <mark@remarkablemark.org>
2022-12-03 20:46:00 -0500

    test(cypress): set up cucumber and add example.feature

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'cypress';
+import createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    specPattern: '**/*.feature',
+    async setupNodeEvents(on, config) {
+      // https://github.com/badeball/cypress-cucumber-preprocessor/blob/master/docs/quick-start.md
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        'file:preprocessor',
+        createBundler({ plugins: [createEsbuildPlugin(config)] })
+      );
+      return config;
     },
   },
 });
---
+++ cypress/e2e/example/example.feature
@@ -0,0 +1,3 @@
+Feature: Example
+  Scenario: Visiting example site
+    When I visit "https://example.com/"
