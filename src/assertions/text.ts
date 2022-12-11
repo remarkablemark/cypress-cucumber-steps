f208ee1f6de6fa0cf36ee83fb3d8124d08c44469
Mark <mark@remarkablemark.org>
2022-12-10 21:37:17 -0500

    fix(assertions): assert "Then I see text" is also visible

--- src/assertions/text.ts
+++ src/assertions/text.ts
@@ -12,9 +12,13 @@ import { Then } from '@badeball/cypress-cucumber-preprocessor';
  * ```gherkin
  * Then I see text "Hello, world!"
  * ```
+ *
+ * @remarks
+ *
+ * This asserts that the text _exists_ and is _visible_ in the screen.
  */
 export function Then_I_see_text(text: string) {
-  cy.contains(text).should('exist');
+  cy.contains(text).should('exist').and('be.visible');
 }
 
 Then('I see text {string}', Then_I_see_text);
@@ -29,8 +33,12 @@ Then('I see text {string}', Then_I_see_text);
  * @example
  *
  * ```gherkin
- * Then I do not see text "Hello, world!"
+ * Then I do not see text "I do not exist"
  * ```
+ *
+ * @remarks
+ *
+ * This asserts that the text _does not exist_ in the screen.
  */
 export function Then_I_do_not_see_text(text: string) {
   cy.contains(text).should('not.exist');
