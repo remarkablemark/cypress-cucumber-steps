ecd381d17f837834b91d3bcf83b38ad4a83e01d3
Mark <mark@remarkablemark.org>
2022-12-11 01:07:18 -0500

    feat(actions): add "When I click"

--- src/actions/click.ts
+++ src/actions/click.ts
@@ -1,5 +1,35 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
+import { getCypressElement } from '../utils';
+
+/**
+ * When I click:
+ *
+ * ```gherkin
+ * When I click
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I click
+ * ```
+ *
+ * @remarks
+ *
+ * This requires a preceding step like {@link When_I_find_element_by_text | "When I find element by text"}. For example:
+ *
+ * ```gherkin
+ * When I find element by text "Click Me"
+ *   And I click
+ * ```
+ */
+export function When_I_click() {
+  getCypressElement(this).click();
+}
+
+When('I click', When_I_click);
+
 /**
  * When I click on text:
  *
