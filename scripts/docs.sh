f962d7fc8f378412b2194c15dedc778971beeaa1
Mark <mark@remarkablemark.org>
2022-12-11 17:27:28 -0500

    build(scripts): prepend BASE_URL to paths in 404.html

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -13,12 +13,24 @@ README_END='<!-- readme-content-end -->'
 README_PLACEHOLDER='<!-- readme-content-placeholder -->'
 README_REPLACEMENT='<h1>Not Found</h1>'
 
+if [[ $CF_PAGES == '1' ]]; then
+  BASE_URL='/'
+fi
+
 if [[ $(uname) == 'Linux' ]]; then
   sed -i "/$README_START/,/$README_END/d" $NOT_FOUND_PATH
   sed -i "s|$README_PLACEHOLDER|$README_REPLACEMENT|" $NOT_FOUND_PATH
+
+  for path in 'assets/' 'functions/' 'interfaces/' 'modules/' 'types/'; do
+    sed -i "s|$path|${BASE_URL}${path}|g" $NOT_FOUND_PATH
+  done
 elif [[ $(uname) == 'Darwin' ]]; then
   sed -i '' "/$README_START/,/$README_END/d" $NOT_FOUND_PATH
   sed -i '' "s|$README_PLACEHOLDER|$README_REPLACEMENT|" $NOT_FOUND_PATH
+
+  for path in 'assets/' 'functions/' 'interfaces/' 'modules/' 'types/'; do
+    sed -i '' "s|$path|${BASE_URL}${path}|g" $NOT_FOUND_PATH
+  done
 fi
 
 echo "Created $NOT_FOUND_PATH"
@@ -35,9 +47,9 @@ if [[ $CF_PAGES == '1' ]]; then
   mv $FUNCTIONS_DIRECTORY $RENAMED_FUNCTIONS_DIRECTORY
 
   if [[ $(uname) == 'Linux' ]]; then
-    sed -i "s/href=\"$FUNCTIONS/href=\"$RENAMED_FUNCTIONS/g" $DOCS_DIRECTORY/*.html
+    sed -i "s|$FUNCTIONS/|$RENAMED_FUNCTIONS/|g" $DOCS_DIRECTORY/*.html
   elif [[ $(uname) == 'Darwin' ]]; then
-    sed -i '' "s/href=\"$FUNCTIONS/href=\"$RENAMED_FUNCTIONS/g" $DOCS_DIRECTORY/*.html
+    sed -i '' "s|$FUNCTIONS/|$RENAMED_FUNCTIONS/|g" $DOCS_DIRECTORY/*.html
   fi
 
   echo "Renamed $FUNCTIONS_DIRECTORY to $RENAMED_FUNCTIONS_DIRECTORY"
