dff3018e1489d3e5eb9ff415549b66b4d4e19d4e
Mark <mark@remarkablemark.org>
2022-12-11 19:23:51 -0500

    feat(assertions): add "Then I do not see visible text"

--- src/assertions/text.ts
+++ src/assertions/text.ts
@@ -45,3 +45,26 @@ export function Then_I_do_not_see_text(text: string) {
 }
 
 Then('I do not see text {string}', Then_I_do_not_see_text);
+
+/**
+ * Then I do not see visible text:
+ *
+ * ```gherkin
+ * Then I do not see visible text {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I do not see visible text "I exist but am hidden"
+ * ```
+ *
+ * @remarks
+ *
+ * This asserts that the text _exists_ in the screen but is _hidden_.
+ */
+export function Then_I_do_not_see_visible_text(text: string) {
+  cy.contains(text).should('not.be.visible');
+}
+
+Then('I do not see visible text {string}', Then_I_do_not_see_visible_text);
