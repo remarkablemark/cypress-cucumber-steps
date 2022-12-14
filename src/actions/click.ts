2b9f45c2318b649f600915109337b04163d8f6c6
Mark <mark@remarkablemark.org>
2022-12-14 13:12:00 -0500

    feat(actions): add "When I double click" & "When I double click on text"

--- src/actions/click.ts
+++ src/actions/click.ts
@@ -9,6 +9,10 @@ import { getCypressElement } from '../utils';
  * When I click
  * ```
  *
+ * Alternative:
+ *
+ * - {@link When_I_click_on_text | When I click on text}
+ *
  * @example
  *
  * ```gherkin
@@ -20,9 +24,14 @@ import { getCypressElement } from '../utils';
  * This requires a preceding step like {@link When_I_find_element_by_text | "When I find element by text"}. For example:
  *
  * ```gherkin
- * When I find element by text "Click Me"
+ * When I find element by text "Click Me Once"
  *   And I click
  * ```
+ *
+ * @see
+ *
+ * - {@link When_I_double_click | When I double-click}
+ * - {@link When_I_double_click_on_text | When I double-click on text}
  */
 export function When_I_click() {
   getCypressElement(this).click();
@@ -37,14 +46,88 @@ When('I click', When_I_click);
  * When I click on text {string}
  * ```
  *
+ * Alternative:
+ *
+ * - {@link When_I_click | When I click}
+ *
  * @example
  *
  * ```gherkin
- * When I click on text "Click Me"
+ * When I click on text "Click Me Once"
  * ```
+ *
+ * @see
+ *
+ * - {@link When_I_double_click | When I double-click}
+ * - {@link When_I_double_click_on_text | When I double-click on text}
  */
 export function When_I_click_on_text(text: string) {
   cy.contains(text).click();
 }
 
 When('I click on text {string}', When_I_click_on_text);
+
+/**
+ * When I double-click:
+ *
+ * ```gherkin
+ * When I double-click
+ * ```
+ *
+ * Alternative:
+ *
+ * - {@link When_I_double_click_on_text | When I double-click on text}
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I double-click
+ * ```
+ *
+ * @remarks
+ *
+ * This requires a preceding step like {@link When_I_find_element_by_text | "When I find element by text"}. For example:
+ *
+ * ```gherkin
+ * When I find element by text "Click Me Twice"
+ *   And I double-click
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_click | When I click}
+ * - {@link When_I_click_on_text | When I click on text}
+ */
+export function When_I_double_click() {
+  getCypressElement(this).dblclick();
+}
+
+When('I double-click', When_I_double_click);
+
+/**
+ * When I double-click on text:
+ *
+ * ```gherkin
+ * When I double-click on text {string}
+ * ```
+ *
+ * Alternative:
+ *
+ * - {@link When_I_double_click | When I double-click}
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I double-click on text "Click Me Twice"
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_click | When I click}
+ * - {@link When_I_click_on_text | When I click on text}
+ */
+export function When_I_double_click_on_text(text: string) {
+  cy.contains(text).dblclick();
+}
+
+When('I double-click on text {string}', When_I_double_click_on_text);
