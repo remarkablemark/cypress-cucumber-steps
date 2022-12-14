5667df7d007d5d1f54e8eb8e7b01be458c6c4366
Mark <mark@remarkablemark.org>
2022-12-14 13:55:22 -0500

    feat(actions): add "When I right click" & "When I right click on text"

--- src/actions/click.ts
+++ src/actions/click.ts
@@ -32,6 +32,8 @@ import { getCypressElement } from '../utils';
  *
  * - {@link When_I_double_click | When I double-click}
  * - {@link When_I_double_click_on_text | When I double-click on text}
+ * - {@link When_I_right_click | When I right-click}
+ * - {@link When_I_right_click_on_text | When I right-click on text}
  */
 export function When_I_click() {
   getCypressElement(this).click();
@@ -60,6 +62,8 @@ When('I click', When_I_click);
  *
  * - {@link When_I_double_click | When I double-click}
  * - {@link When_I_double_click_on_text | When I double-click on text}
+ * - {@link When_I_right_click | When I right-click}
+ * - {@link When_I_right_click_on_text | When I right-click on text}
  */
 export function When_I_click_on_text(text: string) {
   cy.contains(text).click();
@@ -97,6 +101,8 @@ When('I click on text {string}', When_I_click_on_text);
  *
  * - {@link When_I_click | When I click}
  * - {@link When_I_click_on_text | When I click on text}
+ * - {@link When_I_right_click | When I right-click}
+ * - {@link When_I_right_click_on_text | When I right-click on text}
  */
 export function When_I_double_click() {
   getCypressElement(this).dblclick();
@@ -125,9 +131,80 @@ When('I double-click', When_I_double_click);
  *
  * - {@link When_I_click | When I click}
  * - {@link When_I_click_on_text | When I click on text}
+ * - {@link When_I_right_click | When I right-click}
+ * - {@link When_I_right_click_on_text | When I right-click on text}
  */
 export function When_I_double_click_on_text(text: string) {
   cy.contains(text).dblclick();
 }
 
 When('I double-click on text {string}', When_I_double_click_on_text);
+
+/**
+ * When I right-click:
+ *
+ * ```gherkin
+ * When I right-click
+ * ```
+ *
+ * Alternative:
+ *
+ * - {@link When_I_right_click_on_text | When I right-click on text}
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I right-click
+ * ```
+ *
+ * @remarks
+ *
+ * This requires a preceding step like {@link When_I_find_element_by_text | "When I find element by text"}. For example:
+ *
+ * ```gherkin
+ * When I find element by text "Right-Click Me"
+ *   And I right-click
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_click | When I click}
+ * - {@link When_I_click_on_text | When I click on text}
+ * - {@link When_I_double_click | When I double-click}
+ * - {@link When_I_double_click_on_text | When I double-click on text}
+ */
+export function When_I_right_click() {
+  getCypressElement(this).rightclick();
+}
+
+When('I right-click', When_I_right_click);
+
+/**
+ * When I right-click on text:
+ *
+ * ```gherkin
+ * When I right-click on text {string}
+ * ```
+ *
+ * Alternative:
+ *
+ * - {@link When_I_right_click | When I right-click}
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I right-click on text "Right-Click Me"
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_click | When I click}
+ * - {@link When_I_click_on_text | When I click on text}
+ * - {@link When_I_double_click | When I double-click}
+ * - {@link When_I_double_click_on_text | When I double-click on text}
+ */
+export function When_I_right_click_on_text(text: string) {
+  cy.contains(text).rightclick();
+}
+
+When('I right-click on text {string}', When_I_right_click_on_text);
