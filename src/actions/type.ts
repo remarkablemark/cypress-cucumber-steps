e4d4f4d6f4bbf1ee780436d040526cc08e13e891
Mark <mark@remarkablemark.org>
2022-12-15 18:33:41 -0500

    feat(actions): add "When I clear"

--- src/actions/type.ts
+++ src/actions/type.ts
@@ -3,6 +3,40 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
 import { getCypressElement } from '../utils';
 
 /**
+ * When I clear:
+ *
+ * ```gherkin
+ * When I clear
+ * ```
+ *
+ * Alias for {@link When_I_type | "When I type"}:
+ *
+ * ```gherkin
+ * When I type "{selectall}{backspace}"
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I clear
+ * ```
+ *
+ * @remarks
+ *
+ * This requires a preceding step like {@link When_I_get_element_by_label_text | "When I get element by label text"}. For example:
+ *
+ * ```gherkin
+ * When I get element by label text "Input"
+ *   And I clear
+ * ```
+ */
+export function When_I_clear() {
+  getCypressElement(this).clear();
+}
+
+When('I clear', When_I_clear);
+
+/**
  * When I type:
  *
  * ```gherkin
