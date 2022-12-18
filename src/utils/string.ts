2830fcaf98d6447fef88b079852f71cbf2b30328
Mark <mark@remarkablemark.org>
2022-12-18 14:47:43 -0500

    feat(utils): create private helper camelCase

---
+++ src/utils/string.ts
@@ -0,0 +1,20 @@
+/* eslint-disable tsdoc/syntax */
+const HYPHEN_REGEX = /-([a-z])/g;
+
+/**
+ * Replacer that capitalizes the first character in a string match.
+ *
+ * @private
+ */
+function capitalize(match: string, character: string): string {
+  return character.toUpperCase();
+}
+
+/**
+ * Transforms a string to camelCase.
+ *
+ * @private
+ */
+export function camelCase(text: string): string {
+  return text.replace(HYPHEN_REGEX, capitalize);
+}
