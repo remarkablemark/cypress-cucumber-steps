9c4cb65c6e7f36b08885197747abb8255baf2843
Mark <mark@remarkablemark.org>
2023-01-01 22:44:16 -0500

    feat(assertions): add "Then I see label"

--- src/assertions/index.ts
+++ src/assertions/index.ts
@@ -2,6 +2,7 @@ export * from './button';
 export * from './element';
 export * from './hash';
 export * from './heading';
+export * from './label';
 export * from './link';
 export * from './text';
 export * from './url';
---
+++ src/assertions/label.ts
@@ -0,0 +1,40 @@
+import { Then } from '@badeball/cypress-cucumber-preprocessor';
+
+/**
+ * Then I see label:
+ *
+ * ```gherkin
+ * Then I see label {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I see label "Label"
+ * ```
+ *
+ * @remarks
+ *
+ * This asserts that a label with text **_exists_** and is **_visible_** in the screen.
+ *
+ * Inspired by Testing Library's [ByLabelText](https://testing-library.com/docs/queries/bylabeltext).
+ *
+ * @see
+ *
+ * - {@link Then_I_see_text | Then I see text}
+ */
+export function Then_I_see_label(text: string) {
+  cy.get('body').then(($body) => {
+    if ($body.find('label:visible').text().includes(text)) {
+      cy.contains('label:visible', text).should('exist');
+    } else if (!$body.find(`[aria-labelledby='${text}']:visible`).length) {
+      cy.contains(`[aria-labelledby='${text}']:visible`, text).should('exist');
+    } else if (!$body.find(`[aria-label='${text}']:visible`).length) {
+      cy.contains(`[aria-label='${text}']:visible`, text).should('exist');
+    } else {
+      throw new Error(`Unable to see label: ${text}`);
+    }
+  });
+}
+
+Then('I see label {string}', Then_I_see_label);
