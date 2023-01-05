ee63f061d44136ec53e343569dd29bcd8feb6fa4
Mark <mark@remarkablemark.org>
2023-01-04 20:21:54 -0500

    feat(queries): add "When I find element by title"

--- src/queries/title.ts
+++ src/queries/title.ts
@@ -2,15 +2,53 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
 
 import { setCypressElement } from '../utils';
 
+/**
+ * When I find element by title:
+ *
+ * ```gherkin
+ * When I find element by title {string}
+ * ```
+ *
+ * Finds the first element that has a matching `title` attribute. This will also find a `title` element within an SVG.
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I find element by title "Close"
+ * ```
+ *
+ * @remarks
+ *
+ * This precedes steps like {@link When_I_click | "When I click"}. For example:
+ *
+ * ```gherkin
+ * When I find element by title "Close"
+ *   And I click
+ * ```
+ *
+ * Inspired by Testing Library's [ByTitle](https://testing-library.com/docs/queries/bytitle).
+ */
+export function When_I_find_element_by_title(title: string) {
+  const selector = [
+    `svg title:contains('${title}')`,
+    `[title='${title}']`,
+  ].join(',');
+  setCypressElement(cy.get(selector).first());
+}
+
+When('I find element by title {string}', When_I_find_element_by_title);
+
 /* eslint-disable tsdoc/syntax */
 /**
+ * # _Deprecated_
+ *
  * When I get element by title:
  *
  * ```gherkin
  * When I get element by title {string}
  * ```
  *
- * Gets the element that has the matching `title` attribute. This will also find a `title` element within an SVG.
+ * Gets the first element that has a matching `title` attribute. This will also find a `title` element within an SVG.
  *
  * > This query will throw an error if no element is found and will not wait and retry.
  *
@@ -30,6 +68,8 @@ import { setCypressElement } from '../utils';
  * ```
  *
  * Inspired by Testing Library's [ByTitle](https://testing-library.com/docs/queries/bytitle).
+ *
+ * @deprecated Use {@link When_I_find_element_by_title | "When I find element by title"} instead.
  */
 /* eslint-enable tsdoc/syntax */
 export function When_I_get_element_by_title(title: string) {
