9fef794614fd175aa6e356e4deaa02b3296dceed
Mark <mark@remarkablemark.org>
2023-03-05 15:21:57 -0500

    feat(assertions): add "I see document title" & "I see document contains"
    https://docs.cypress.io/api/commands/title

---
+++ src/assertions/document-title.ts
@@ -0,0 +1,54 @@
+import { Then } from '@badeball/cypress-cucumber-preprocessor';
+
+/**
+ * Then I see document title:
+ *
+ * ```gherkin
+ * Then I see document title {string}
+ * ```
+ *
+ * @example
+ *
+ * Assert that the `document.title` property of the page is "Title":
+ *
+ * ```gherkin
+ * Then I see document title "Title"
+ * ```
+ *
+ * @see
+ *
+ * - {@link Then_I_see_document_title_contains | Then I see document title contains}
+ */
+export function Then_I_see_document_title(title: string) {
+  cy.title().should('equal', title);
+}
+
+Then('I see document title {string}', Then_I_see_document_title);
+
+/**
+ * Then I see document title contains:
+ *
+ * ```gherkin
+ * Then I see document title contains {string}
+ * ```
+ *
+ * @example
+ *
+ * Assert that the document's title contains "Title":
+ *
+ * ```gherkin
+ * Then I see document title contains "Title"
+ * ```
+ *
+ * @see
+ *
+ * - {@link Then_I_see_document_title | Then I see document title}
+ */
+export function Then_I_see_document_title_contains(title: string) {
+  cy.title().should('contain', title);
+}
+
+Then(
+  'I see document title contains {string}',
+  Then_I_see_document_title_contains
+);
