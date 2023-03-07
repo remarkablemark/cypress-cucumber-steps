c6208d7c2c33a69cc776fc748c3f795ba68fe4a5
Mark <mark@remarkablemark.org>
2023-03-06 22:22:48 -0500

    feat(assertions): add "Then I see element has attribute containing"

--- src/assertions/attribute.ts
+++ src/assertions/attribute.ts
@@ -27,6 +27,7 @@ import { getCypressElement } from '../utils';
  * @see
  *
  * - {@link Then_I_see_element_has_attribute_equal_to | Then I see element has attribute equal to}
+ * - {@link Then_I_see_element_has_attribute_containing | Then I see element has attribute containing}
  */
 export function Then_I_see_element_has_attribute(attributeName: string) {
   getCypressElement().should('have.attr', attributeName);
@@ -59,6 +60,7 @@ Then('I see element has attribute {string}', Then_I_see_element_has_attribute);
  * @see
  *
  * - {@link Then_I_see_element_has_attribute | Then I see element has attribute}
+ * - {@link Then_I_see_element_has_attribute_containing | Then I see element has attribute containing}
  */
 export function Then_I_see_element_has_attribute_equal_to(
   attributeName: string,
@@ -70,4 +72,45 @@ export function Then_I_see_element_has_attribute_equal_to(
 Then(
   'I see element has attribute {string} equal to {string}',
   Then_I_see_element_has_attribute_equal_to
+);
+
+/**
+ * Then I see element has attribute containing:
+ *
+ * ```gherkin
+ * Then I see element has attribute {string} containing {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I see element has attribute "name" containing "value"
+ * ```
+ *
+ * @remarks
+ *
+ * A preceding step like {@link When_I_find_element_by_text | "When I find element by text"} is required. For example:
+ *
+ * ```gherkin
+ * When I find link by text "Link"
+ * Then I see element has attribute "href" containing "/"
+ * ```
+ *
+ * @see
+ *
+ * - {@link Then_I_see_element_has_attribute | Then I see element has attribute}
+ * - {@link Then_I_see_element_has_attribute_equal_to | Then I see element has attribute equal to}
+ */
+export function Then_I_see_element_has_attribute_containing(
+  attributeName: string,
+  attributeValue: string
+) {
+  getCypressElement()
+    .should('have.attr', attributeName)
+    .and('include', attributeValue);
+}
+
+Then(
+  'I see element has attribute {string} containing {string}',
+  Then_I_see_element_has_attribute_containing
 );
