18b8a91de205e66868da36b5985451b2fbf3eae7
Mark <mark@remarkablemark.org>
2023-07-09 20:16:22 -0400

    feat(assertions): add "Then I see response body"

--- src/assertions/response.ts
+++ src/assertions/response.ts
@@ -31,3 +31,37 @@ export function Then_I_see_response_status(code: number) {
 }
 
 Then('I see response status {int}', Then_I_see_response_status);
+
+/**
+ * Then I see response body:
+ *
+ * ```gherkin
+ * Then I see response body {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I see response body "OK
+ * ```
+ *
+ * @remarks
+ *
+ * A preceding step like {@link When_I_make_a_request | "When I make a request"} is required. For example:
+ *
+ * ```gherkin
+ * When I make a "GET" request to "/user.json"
+ * Then I see response body '{"name":"Mark"}'
+ * ```
+ */
+export function Then_I_see_response_body(body: string) {
+  getCypressElement().should((response: Cypress.Response<object>) => {
+    if (typeof response.body === 'object') {
+      expect(response.body).to.eql(JSON.parse(body));
+    } else {
+      expect(response.body).to.eq(body);
+    }
+  });
+}
+
+Then('I see response body {string}', Then_I_see_response_body);
