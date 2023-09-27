bbc2d101cfcd27826c757163158785a197898aac
Mark <mark@remarkablemark.org>
2023-09-26 21:35:32 -0400

    feat(actions): add "When I select"

--- src/actions/select.ts
+++ src/actions/select.ts
@@ -1,5 +1,39 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
+import { getCypressElement } from '../utils';
+
+/**
+ * When I select:
+ *
+ * ```gherkin
+ * When I select {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I select "Option"
+ * ```
+ *
+ * @remarks
+ *
+ * A preceding step like {@link When_I_get_element_by_selector | "When I get element by selector"} is required. For example:
+ *
+ * ```gherkin
+ * When I get element by selector "select"
+ *   And I select "Option"
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_select_option | When I select option}
+ */
+export function When_I_select(displayValue: string) {
+  getCypressElement().select(displayValue);
+}
+
+When('I select {string}', When_I_select);
+
 /**
  * When I select option:
  *
@@ -10,12 +44,16 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
  * @example
  *
  * ```gherkin
- * When I select option "Medium"
+ * When I select option "Option"
  * ```
  *
  * @remarks
  *
  * Selects the first option by display value.
+ *
+ * @see
+ *
+ * - {@link When_I_select | When I select}
  */
 export function When_I_select_option(displayValue: string) {
   cy.get('option')
