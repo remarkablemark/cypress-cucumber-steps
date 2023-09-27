cb8ea04f331f1c3179b7706e761e9ffc47d1415e
Mark <mark@remarkablemark.org>
2023-09-26 22:03:57 -0400

    build(scripts): fix Cloudflare Pages `/functions` directory

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -13,6 +13,7 @@ README_END='<!-- readme-content-end -->'
 README_PLACEHOLDER='<!-- readme-content-placeholder -->'
 README_REPLACEMENT='<h1>Not Found</h1>'
 
+# https://developers.cloudflare.com/pages/platform/build-configuration/#environment-variables
 if [[ $CF_PAGES == '1' ]]; then
   BASE_URL='/'
 fi
@@ -35,24 +36,15 @@ fi
 
 echo "Created $NOT_FOUND_PATH"
 
-# https://developers.cloudflare.com/pages/platform/build-configuration/#environment-variables
+# https://community.cloudflare.com/t/functions-dir-is-ignored-in-deploy/438211
+# https://github.com/cloudflare/workers-sdk/issues/2240
 if [[ $CF_PAGES == '1' ]]; then
-  # https://community.cloudflare.com/t/functions-dir-is-ignored-in-deploy/438211
-  # https://github.com/cloudflare/wrangler2/issues/2240#issuecomment-1343406897
-  FUNCTIONS='functions/'
-  RENAMED_FUNCTIONS='function/'
-  FUNCTIONS_DIRECTORY="$DOCS_DIRECTORY/$FUNCTIONS"
-  RENAMED_FUNCTIONS_DIRECTORY="$DOCS_DIRECTORY/$RENAMED_FUNCTIONS"
+  mv $DOCS_DIRECTORY/functions $DOCS_DIRECTORY/function
+  echo "Renamed /functions directory"
 
-  mv $FUNCTIONS_DIRECTORY $RENAMED_FUNCTIONS_DIRECTORY
-
-  if [[ $(uname) == 'Linux' ]]; then
-    sed -i "s|$FUNCTIONS|$RENAMED_FUNCTIONS|g" $DOCS_DIRECTORY/*.html
-    sed -i "s|$FUNCTIONS|$RENAMED_FUNCTIONS|g" $DOCS_DIRECTORY/assets/search.js
-  elif [[ $(uname) == 'Darwin' ]]; then
-    sed -i '' "s|$FUNCTIONS|$RENAMED_FUNCTIONS|g" $DOCS_DIRECTORY/*.html
-    sed -i '' "s|$FUNCTIONS|$RENAMED_FUNCTIONS|g" $DOCS_DIRECTORY/assets/search.js
-  fi
-
-  echo "Renamed $FUNCTIONS_DIRECTORY to $RENAMED_FUNCTIONS_DIRECTORY"
+  # https://developers.cloudflare.com/pages/platform/redirects/
+  cat > $DOCS_DIRECTORY/_redirects <<EOL
+/functions/* /function/:splat
+EOL
+  echo "Created _redirects"
 fi
