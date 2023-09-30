662dca1411e1ddce3127e61b99adefe7b4303160
Mark <mark@remarkablemark.org>
2023-09-30 12:24:10 -0400

    feat(queries): add "When I find images by alt text"

--- src/queries/alt-texts.ts
+++ src/queries/alt-texts.ts
@@ -1,6 +1,6 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
-import { setCypressElement } from '../utils';
+import { getCypressElement, setCypressElement } from '../utils';
 
 /**
  * When I find elements by alt text:
@@ -9,7 +9,7 @@ import { setCypressElement } from '../utils';
  * When I find elements by alt text {string}
  * ```
  *
- * This finds elements (`<img>`) that match the `alt` text.
+ * Finds elements (e.g., `<img>`) that match the `alt` text.
  *
  * @example
  *
@@ -22,7 +22,7 @@ import { setCypressElement } from '../utils';
  * This precedes steps like {@link When_I_click | "When I click"}. For example:
  *
  * ```gherkin
- * When I find elements by alt text "Image Description"
+ * When I find elements by alt text "Text"
  *   And I get 1st element
  *   And I click
  * ```
@@ -32,9 +32,49 @@ import { setCypressElement } from '../utils';
  * @see
  *
  * - {@link When_I_find_element_by_alt_text | When I find element by alt text}
+ * - {@link When_I_find_images_by_alt_text | When I find images by alt text}
  */
 export function When_I_find_elements_by_alt_text(altText: string) {
   setCypressElement(cy.get(`[alt='${altText}']:visible`));
 }
 
 When('I find elements by alt text {string}', When_I_find_elements_by_alt_text);
+
+/**
+ * When I find images by alt text:
+ *
+ * ```gherkin
+ * When I find images by alt text {string}
+ * ```
+ *
+ * Finds all `<img>` that match the `alt` text.
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I find images by alt text "Text"
+ * ```
+ *
+ * @remarks
+ *
+ * This precedes steps like {@link When_I_click | "When I click"}. For example:
+ *
+ * ```gherkin
+ * When I find images by alt text "Text"
+ *   And I get 1st element
+ *   And I click
+ * ```
+ *
+ * Inspired by Testing Library's [ByAltText](https://testing-library.com/docs/queries/byalttext).
+ *
+ * @see
+ *
+ * - {@link When_I_find_elements_by_alt_text | When I find elements by alt text}
+ * - {@link When_I_find_image_by_alt_text | When I find image by alt text}
+ */
+export function When_I_find_images_by_alt_text(altText: string) {
+  When_I_find_elements_by_alt_text(altText);
+  setCypressElement(getCypressElement().filter('img'));
+}
+
+When('I find images by alt text {string}', When_I_find_images_by_alt_text);
