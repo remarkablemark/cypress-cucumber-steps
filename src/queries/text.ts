705324d72de5ec73dd61a1143ca4f67f4c828fce
Mark <mark@remarkablemark.org>
2023-10-01 15:03:32 -0400

    feat(queries): add "When I find elements by text"

--- src/queries/text.ts
+++ src/queries/text.ts
@@ -1,6 +1,41 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
-import { setCypressElement } from '../utils';
+import { getCypressElement, setCypressElement } from '../utils';
+
+/**
+ * When I find elements by text:
+ *
+ * ```gherkin
+ * When I find elements by text {string}
+ * ```
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I find elements by text "Text"
+ * ```
+ *
+ * @remarks
+ *
+ * This precedes steps like {@link When_I_click | "When I click"}. For example:
+ *
+ * ```gherkin
+ * When I find elements by text "Text"
+ *   And I get 1st element
+ *   And I click
+ * ```
+ *
+ * Inspired by Testing Library's [ByText](https://testing-library.com/docs/queries/bytext).
+ *
+ * @see
+ *
+ * - {@link When_I_find_element_by_text | When I find element by text}
+ */
+export function When_I_find_elements_by_text(text: string) {
+  setCypressElement(cy.contains(text));
+}
+
+When('I find elements by text {string}', When_I_find_elements_by_text);
 
 /**
  * When I find element by text:
@@ -28,11 +63,11 @@ import { setCypressElement } from '../utils';
  *
  * @see
  *
- * - {@link When_I_find_button_by_text | When I find button by text}
- * - {@link When_I_find_link_by_text | When I find link by text}
+ * - {@link When_I_find_elements_by_text | When I find elements by text}
  */
 export function When_I_find_element_by_text(text: string) {
-  setCypressElement(cy.contains(text));
+  When_I_find_elements_by_text(text);
+  setCypressElement(getCypressElement().first());
 }
 
 When('I find element by text {string}', When_I_find_element_by_text);
