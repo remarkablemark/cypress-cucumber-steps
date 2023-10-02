0ee77226584b4d360dd1bce780b332ae7a9b371c
Mark <mark@remarkablemark.org>
2023-10-01 20:43:16 -0400

    feat(queries): add "When I find heading by text"

--- src/queries/heading.ts
+++ src/queries/heading.ts
@@ -1,6 +1,6 @@
 import { When } from '@badeball/cypress-cucumber-preprocessor';
 
-import { setCypressElement } from '../utils';
+import { getCypressElement, setCypressElement } from '../utils';
 
 /**
  * When I find headings by text:
@@ -37,3 +37,38 @@ export function When_I_find_headings_by_text(text: string) {
 }
 
 When('I find headings by text {string}', When_I_find_headings_by_text);
+
+/**
+ * When I find heading by text:
+ *
+ * ```gherkin
+ * When I find heading by text {string}
+ * ```
+ *
+ * Finds first heading element that matches text.
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I find heading by text "Heading"
+ * ```
+ *
+ * @remarks
+ *
+ * This precedes steps like {@link When_I_click | "When I click"}. For example:
+ *
+ * ```gherkin
+ * When I find heading by text "heading"
+ *   And I click
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_find_headings_by_text | When I find headings by text}
+ */
+export function When_I_find_heading_by_text(text: string) {
+  When_I_find_headings_by_text(text);
+  setCypressElement(getCypressElement().first());
+}
+
+When('I find heading by text {string}', When_I_find_heading_by_text);
