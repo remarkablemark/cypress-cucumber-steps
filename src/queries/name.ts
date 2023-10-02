2a4c4893f1cee4dd60002e416fe50982a3f4c660
Mark <mark@remarkablemark.org>
2023-10-02 01:02:10 -0400

    feat(queries): add "When I find input by name"

--- src/queries/name.ts
+++ src/queries/name.ts
@@ -113,3 +113,40 @@ export function When_I_find_inputs_by_name(name: string) {
 }
 
 When('I find inputs by name {string}', When_I_find_inputs_by_name);
+
+/* eslint-disable tsdoc/syntax */
+/**
+ * When I find input by name:
+ *
+ * ```gherkin
+ * When I find input by name {string}
+ * ```
+ *
+ * > A name is not a good substitute for a label so you should generally use {@link When_I_find_element_by_label_text | "When I find element by label text"} instead.
+ *
+ * @example
+ *
+ * ```gherkin
+ * When I find input by name "text"
+ * ```
+ *
+ * @remarks
+ *
+ * This precedes steps like {@link When_I_type | "When I type"}. For example:
+ *
+ * ```gherkin
+ * When I find input by name "password"
+ *   And I type "password"
+ * ```
+ *
+ * @see
+ *
+ * - {@link When_I_find_elements_by_name | When I find elements by name}
+ */
+/* eslint-enable tsdoc/syntax */
+export function When_I_find_input_by_name(name: string) {
+  When_I_find_inputs_by_name(name);
+  setCypressElement(getCypressElement().first());
+}
+
+When('I find input by name {string}', When_I_find_input_by_name);
