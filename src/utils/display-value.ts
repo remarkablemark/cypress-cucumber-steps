ed103716d2bb38bfe85604dfbe792663d2e2aecd
Mark <mark@remarkablemark.org>
2023-10-07 16:53:58 -0400

    chore(utils): remove getAllByDisplayValue from display-value

--- src/utils/display-value.ts
+++ src/utils/display-value.ts
@@ -1,26 +1,6 @@
 /* eslint-disable tsdoc/syntax */
 
 /**
- * Get all Cypress elements by display value.
- *
- * @param element - Element name.
- * @param value - Display value.
- * @returns - Cypress element.
- * @private
- */
-export function getAllByDisplayValue(
-  element: 'input' | 'textarea',
-  value: string,
-) {
-  return cy
-    .get(`${element}:visible`)
-    .filter(
-      (index, element: HTMLInputElement) =>
-        Cypress.$(element).val()?.toString() === value,
-    );
-}
-
-/**
  * Get first Cypress element by display value.
  *
  * @param element - Element name.
@@ -32,5 +12,11 @@ export function getByDisplayValue(
   element: 'input' | 'textarea',
   value: string,
 ) {
-  return getAllByDisplayValue(element, value).first();
+  return cy
+    .get(`${element}:visible`)
+    .filter(
+      (index, element: HTMLInputElement) =>
+        Cypress.$(element).val()?.toString() === value,
+    )
+    .first();
 }
