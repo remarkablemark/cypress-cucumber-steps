2bc8d5051f5d03ad1d1644ac451ab626721a12b7
Mark <mark@remarkablemark.org>
2023-10-08 14:02:14 -0400

    refactor(assertions): tidy "Then I see heading"

--- src/assertions/heading.ts
+++ src/assertions/heading.ts
@@ -1,13 +1,12 @@
 import { Then } from '@badeball/cypress-cucumber-preprocessor';
 
-import {
-  When_I_find_heading_by_text,
-  When_I_find_headings_by_text,
-} from '../queries';
+import { When_I_find_headings_by_text } from '../queries';
 import { getCypressElement } from '../utils';
 
 /**
  * Then I see heading:
+ *
+ * Assert heading (`h1`, `h2`, `h3`, `h4`, `h5`, or `h6`) with text **_exists_** and is **_visible_** in the screen.
  *
  * ```gherkin
  * Then I see heading {string}
@@ -19,16 +18,12 @@ import { getCypressElement } from '../utils';
  * Then I see heading "Heading"
  * ```
  *
- * @remarks
- *
- * This asserts that a heading (`h1`, `h2`, `h3`, `h4`, `h5`, or `h6`) with text **_exists_** and is **_visible_** in the screen.
- *
  * @see
  *
  * - {@link Then_I_see_text | Then I see text}
  */
 export function Then_I_see_heading(text: string) {
-  When_I_find_heading_by_text(text);
+  When_I_find_headings_by_text(text);
   getCypressElement().should('exist');
 }
 
