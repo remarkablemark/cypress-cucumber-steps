7e3dddb7f9b3f6d291d3b3817af49c49948be559
Mark <mark@remarkablemark.org>
2023-10-08 15:17:37 -0400

    docs(actions): improve blur TypeDoc

--- src/actions/blur.ts
+++ src/actions/blur.ts
@@ -7,6 +7,8 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
  * When I blur
  * ```
  *
+ * Blurs the element with focus.
+ *
  * @example
  *
  * ```gherkin
@@ -15,7 +17,12 @@ import { When } from '@badeball/cypress-cucumber-preprocessor';
  *
  * @remarks
  *
- * Blurs the element with focus.
+ * A preceding step like {@link When_I_find_input_by_label_text | "When I find input by label text"} is required. For example:
+ *
+ * ```gherkin
+ * When I find input by label text "Text"
+ *   And I blur
+ * ```
  */
 export function When_I_blur() {
   cy.focused().blur();
