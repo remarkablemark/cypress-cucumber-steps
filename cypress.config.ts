7301c3f88e423621b9f9be68427d5a79aa395ce5
Mark <mark@remarkablemark.org>
2023-10-08 17:01:37 -0400

    chore(cypress): tidy cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+// @ts-ignore
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+// @ts-ignore
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { defineConfig } from 'cypress';
 
-// https://github.com/badeball/cypress-cucumber-preprocessor/blob/master/docs/quick-start.md
 export default defineConfig({
   e2e: {
     specPattern: '**/*.feature',
-    async setupNodeEvents(on, config) {
+
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions,
+    ): Promise<Cypress.PluginConfigOptions> {
+      // https://github.com/badeball/cypress-cucumber-preprocessor/blob/master/docs/quick-start.md
       await addCucumberPreprocessorPlugin(on, config);
+
       on(
         'file:preprocessor',
-        createBundler({ plugins: [createEsbuildPlugin(config)] }),
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        }),
       );
+
       return config;
     },
   },
+
   retries: {
     runMode: 3,
   },
