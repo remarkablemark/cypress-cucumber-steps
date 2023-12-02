ca9fb19e4b7ca963eceaca8589676696a737e778
Mark <mark@remarkablemark.org>
2023-12-02 15:35:33 -0500

    feat(assertions): add "Then I do not see cookie"

--- src/assertions/cookie.ts
+++ src/assertions/cookie.ts
@@ -9,7 +9,7 @@ import { getOptions } from '../utils';
  * Then I see cookie {string}
  * ```
  *
- * Assert cookie with name exists.
+ * Assert cookie with name **_exists_**.
  *
  * @example
  *
@@ -31,3 +31,33 @@ export function Then_I_see_cookie(name: string, options?: DataTable) {
 }
 
 Then('I see cookie {string}', Then_I_see_cookie);
+
+/**
+ * Then I do not see cookie:
+ *
+ * ```gherkin
+ * Then I do not see cookie {string}
+ * ```
+ *
+ * Assert cookie with name **_does not exist_**.
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I do not see cookie "my-session-cookie"
+ * ```
+ *
+ * With [options](https://docs.cypress.io/api/commands/getcookie#Arguments):
+ *
+ * ```gherkin
+ * Then I do not see cookie "my-session-cookie"
+ *   | domain | example.com |
+ *   | log | true |
+ *   | timeout | 3000 |
+ * ```
+ */
+export function Then_I_do_not_see_cookie(name: string, options?: DataTable) {
+  cy.getCookie(name, getOptions(options)).should('not.exist');
+}
+
+Then('I do not see cookie {string}', Then_I_do_not_see_cookie);
