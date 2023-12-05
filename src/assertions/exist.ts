dff01f1b0b9189b6f08ab1f5a99d33cc6179a84e
Mark <mark@remarkablemark.org>
2023-12-04 20:48:37 -0500

    feat(assertions): add "Then I see element does not exist"

--- src/assertions/exist.ts
+++ src/assertions/exist.ts
@@ -1,6 +1,6 @@
-import { Then } from '@badeball/cypress-cucumber-preprocessor';
+import { DataTable, Then } from '@badeball/cypress-cucumber-preprocessor';
 
-import { getCypressElement } from '../utils';
+import { getCypressElement, getOptions } from '../utils';
 
 /**
  * Then I see element exists:
@@ -17,6 +17,13 @@ import { getCypressElement } from '../utils';
  * Then I see element exists
  * ```
  *
+ * With options:
+ *
+ * ```gherkin
+ * Then I see element exists
+ *   | timeout | 4000 |
+ * ```
+ *
  * @remarks
  *
  * A preceding step like {@link When_I_find_element_by_text | "When I find element by text"} is required. For example:
@@ -25,9 +32,54 @@ import { getCypressElement } from '../utils';
  * When I find element by text "Text"
  * Then I see element exists
  * ```
+ *
+ * @see
+ *
+ * - {@link Then_I_see_element_does_not_exist | Then I see element does not exist}
  */
-export function Then_I_see_element_exists() {
-  getCypressElement().should('exist');
+export function Then_I_see_element_exists(options?: DataTable) {
+  getCypressElement().should('exist', getOptions(options));
 }
 
 Then('I see element exists', Then_I_see_element_exists);
+
+/**
+ * Then I see element does not exist:
+ *
+ * ```gherkin
+ * Then I see element does not exist
+ * ```
+ *
+ * Assert element **_does not exist_** in the screen.
+ *
+ * @example
+ *
+ * ```gherkin
+ * Then I see element does not exist
+ * ```
+ *
+ * With options:
+ *
+ * ```gherkin
+ * Then I see element does not exist
+ *   | timeout | 4000 |
+ * ```
+ *
+ * @remarks
+ *
+ * A preceding step like {@link When_I_get_element_by_selector | "When I get element by selector"} is required. For example:
+ *
+ * ```gherkin
+ * When I get element by selector ".loading"
+ * Then I see element does not exist
+ * ```
+ *
+ * @see
+ *
+ * - {@link Then_I_see_element_exists | Then I see element exists}
+ */
+export function Then_I_see_element_does_not_exist(options?: DataTable) {
+  getCypressElement().should('not.exist', getOptions(options));
+}
+
+Then('I see element does not exist', Then_I_see_element_does_not_exist);
