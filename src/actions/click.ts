7301cc68f86519094185f0a420c38ee8f35ebaaa
Mark <mark@remarkablemark.org>
2023-12-23 17:48:19 -0500

    refactor(actions): use `getLabel in "When I click on label"

--- src/actions/click.ts
+++ src/actions/click.ts
@@ -1,11 +1,11 @@
 import { DataTable, When } from '@badeball/cypress-cucumber-preprocessor';
 
 import { PseudoSelector } from '../constants';
-import { When_I_find_element_by_label_text } from '../queries';
 import {
   camelCase,
   getButtonElements,
   getCypressElement,
+  getLabelElements,
   getLinkElements,
   getOptions,
 } from '../utils';
@@ -338,8 +338,7 @@ When('I click on text {string}', When_I_click_on_text);
  * - {@link When_I_click_on_text | When I click on text}
  */
 export function When_I_click_on_label(text: string, options?: DataTable) {
-  When_I_find_element_by_label_text(text, options);
-  getCypressElement().click();
+  getLabelElements(text, PseudoSelector.visible, options).first().click();
 }
 
 When('I click on label {string}', When_I_click_on_label);
