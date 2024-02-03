8f0b254c166b8cbde05bd09581141b702d5cc62a
Mark <mark@remarkablemark.org>
2024-02-03 14:25:10 -0500

    chore(cypress): tidy cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,33 +1,26 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
-// @ts-ignore
+// @ts-expect-error Cannot find module or its corresponding type declarations.
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
-// @ts-ignore
+// @ts-expect-error Module can only be default-imported using the 'esModuleInterop' flag
 import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     specPattern: '**/*.feature',
-
     async setupNodeEvents(
       on: Cypress.PluginEvents,
       config: Cypress.PluginConfigOptions,
     ): Promise<Cypress.PluginConfigOptions> {
       // https://github.com/badeball/cypress-cucumber-preprocessor/blob/master/docs/quick-start.md
       await addCucumberPreprocessorPlugin(on, config);
-
       on(
         'file:preprocessor',
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        }),
+        createBundler({ plugins: [createEsbuildPlugin(config)] }),
       );
-
       return config;
     },
   },
-
   retries: {
     runMode: 3,
   },
